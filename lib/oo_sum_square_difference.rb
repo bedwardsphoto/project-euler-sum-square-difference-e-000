154fa1bf5a961c97bb3349ae8d78a0ae81ee25b0
bedwardsphoto <brian@brianedwardsphotography.com>
2016-01-31 20:43:19 -0500

    Done.

--- lib/oo_sum_square_difference.rb
+++ lib/oo_sum_square_difference.rb
@@ -1,1 +1,20 @@
-# Implement your object-oriented solution here!
+class SumSquareDifference
+def initialize(input)
+  @input=input
+end
+def input=(input)
+end
+def input
+    @input
+end
+def difference
+  sum_square=0
+  sum=0
+    (1..@input).each do |x|
+    sum_square+= x**2
+    end
+  ((1..@input).each {|a| sum+=a})
+  difference= (sum**2)-sum_square
+    return difference
+  end
+end
--- lib/sum_square_difference.rb
+++ lib/sum_square_difference.rb
@@ -1,1 +1,11 @@
-# Implement your procedural solution here!
+def sum_square_difference(number)
+sum_square=0
+sum=0
+(1..number).each do |x|
+  sum_square+= x**2
+end
+((1..number).each {|a| sum+=a})
+difference= (sum**2)-sum_square
+return difference
+end
+
